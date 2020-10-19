5f0b3d74fd44b8397c01978288c40d09ba12b2e8
Kaatinga <painter@3lines.club>
2020-10-19 16:00:49 +0300

    logger is initialized now in NewConfig()

--- main.go
+++ main.go
@@ -1,6 +1,14 @@
 package QuickHTTPServerLauncher
 
+import (
+	"github.com/kaatinga/bufferedlogger"
+	"os"
+)
+
 func NewConfig() (config Config) {
+
+	// Setting up logger
+	logger := bufferedlogger.InitLog(os.Stdout)
+	config.logger = &logger
 	return
 }
-
