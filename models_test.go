0b7f504d9c20c0864e992a050ba047b3ce8422ed
Michael Gunkoff <painter@3lines.club>
2021-06-20 05:00:54 +0300

    unit test configs were commented

--- models_test.go
+++ models_test.go
@@ -1,69 +1,69 @@
 package httpeasy
 
 var (
-	validConfig = Config{
-		SSL: &SSL{Email: "info@yandex.ru",
-			Domain: "yandex.ru",
-		},
-		ProductionMode: true,
-		HTTP: HTTP{
-			Port: 8089,
-		},
-	}
-
-	portTooSmall = Config{
-		SSL: &SSL{Email: "info@yandex.ru",
-			Domain: "yandex.ru",
-		},
-		ProductionMode: true,
-		HTTP: HTTP{
-			Port: 50,
-		},
-	}
-
-	portTooBig = Config{
-		SSL: &SSL{Email: "info@yandex.ru",
-			Domain: "yandex.ru",
-		},
-		ProductionMode: true,
-		HTTP: HTTP{
-			Port: 50000,
-		},
-	}
-
-	badEmail = Config{
-		SSL: &SSL{Email: "info",
-			Domain: "yandex.ru",
-		},
-		ProductionMode: true,
-		HTTP: HTTP{
-			Port: 8089,
-		},
-	}
-
-	badDomain = Config{
-		SSL: &SSL{Email: "info@yandex.ru",
-			Domain: "-",
-		},
-		ProductionMode: true,
-		HTTP: HTTP{
-			Port: 8089,
-		},
-	}
-
-	devMode = Config{
-		HTTP: HTTP{
-			Port: 8089,
-		},
-	}
-
-	sslForgotten = Config{
-		ProductionMode: true,
-		HTTP:           HTTP{Port: 8089},
-	}
-
-	dbForgotten = Config{
-		HasDB: true,
-		HTTP:  HTTP{Port: 8089},
-	}
+	//validConfig = Config{
+	//	SSL: &SSL{Email: "info@yandex.ru",
+	//		Domain: "yandex.ru",
+	//	},
+	//	ProductionMode: true,
+	//	HTTP: HTTP{
+	//		Port: 8089,
+	//	},
+	//}
+	//
+	//portTooSmall = Config{
+	//	SSL: &SSL{Email: "info@yandex.ru",
+	//		Domain: "yandex.ru",
+	//	},
+	//	ProductionMode: true,
+	//	HTTP: HTTP{
+	//		Port: 50,
+	//	},
+	//}
+	//
+	//portTooBig = Config{
+	//	SSL: &SSL{Email: "info@yandex.ru",
+	//		Domain: "yandex.ru",
+	//	},
+	//	ProductionMode: true,
+	//	HTTP: HTTP{
+	//		Port: 50000,
+	//	},
+	//}
+	//
+	//badEmail = Config{
+	//	SSL: &SSL{Email: "info",
+	//		Domain: "yandex.ru",
+	//	},
+	//	ProductionMode: true,
+	//	HTTP: HTTP{
+	//		Port: 8089,
+	//	},
+	//}
+	//
+	//badDomain = Config{
+	//	SSL: &SSL{Email: "info@yandex.ru",
+	//		Domain: "-",
+	//	},
+	//	ProductionMode: true,
+	//	HTTP: HTTP{
+	//		Port: 8089,
+	//	},
+	//}
+	//
+	//devMode = Config{
+	//	HTTP: HTTP{
+	//		Port: 8089,
+	//	},
+	//}
+	//
+	//sslForgotten = Config{
+	//	ProductionMode: true,
+	//	HTTP:           HTTP{Port: 8089},
+	//}
+	//
+	//dbForgotten = Config{
+	//	HasDB: true,
+	//	HTTP:  HTTP{Port: 8089},
+	//}
 )
