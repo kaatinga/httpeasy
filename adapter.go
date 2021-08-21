4a4a99e1a6f1348d06122d104a99b848604dfa43
Michael Gunkoff <painter@3lines.club>
2021-08-21 10:44:52 +0300

    added Adapter to create handlers chain

---
+++ adapter.go
@@ -0,0 +1,14 @@
+package httpeasy
+
+import "github.com/julienschmidt/httprouter"
+
+
+type Adapter func(httprouter.Handle) httprouter.Handle
+
+// Adapt composes handlers chain end executes it.
+func Adapt(next httprouter.Handle, adapters ...Adapter) httprouter.Handle {
+	for _, adapter := range adapters {
+		next = adapter(next)
+	}
+	return next
+}
