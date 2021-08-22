2b4d4bc36e2fa1a49a8556bdd38ef97eed589439
Michael Gunkoff <painter@3lines.club>
2021-08-22 14:36:00 +0300

    added custom immutable errors

--- errors.go
+++ errors.go
@@ -1,2 +1,8 @@
 package httpeasy
 
+import cerr "github.com/kaatinga/const-errs"
+
+const (
+	ErrRouterTypeIsIncorrect = cerr.Error("Incorrect Router Type")
+	ErrLoggerIsNotEnabled    = cerr.Error("Incorrect Config Logger")
+)
