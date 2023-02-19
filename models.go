d66fb29f3b084bb1e481217e6402c269ff4abc29
Michael Gunkoff <painter@3lines.club>
2023-02-19 17:59:08 +0100

    clean up

--- models.go
+++ models.go
@@ -113,7 +113,7 @@ func (config *Config) Launch(handlers SetUpHandlers) error {
 		}()
 	}
 
-	interrupt := make(chan os.Signal)
+	interrupt := make(chan os.Signal, 1)
 	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
 
 	var terminationSignal string
